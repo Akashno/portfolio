402680576daef32c35e349abf557b92e1bd93239
Akashno <52347258+Akashno@users.noreply.github.com>
2020-10-29 16:26:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let theme = localStorage.getItem('theme')
+if (theme = null)
+{
+    setTheme('pink')
+}
+else{
+    setTheme(theme)
+}
+let themeDots = document.getElementsByClassName('theme-dot')
+for(var i=0;themeDots.length>i;i++){
+    themeDots[i].addEventListener('click',function(){
+        let mode = this.dataset.mode
+        setTheme(mode)
+    })
+}
+function setTheme(mode){
+    if(mode == 'pink'){
+        document.getElementById('theme-style').href='default.css'
+    }
+    if(mode == 'blue'){
+        document.getElementById('theme-style').href='blue.css'
+    }
+    if(mode == 'yellow'){
+        document.getElementById('theme-style').href='yellow.css'
+    }
+    localStorage.setItem('theme',mode)
+    
+}
